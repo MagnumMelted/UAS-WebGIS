9ad8600d4456d7c345f80b3184d5242e98c8460f
MagnumMelted <egaortega13@gmail.com>
2023-04-28 14:59:08 +0700

    Melakukan Langkah ke- 9

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] =  "";
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +34,7 @@ $config['base_url'] =  "";
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
